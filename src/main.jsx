c2e8233527c6878cdebea9a4853794301ee921f3
maayins <70481844+maayins@users.noreply.github.com>
2026-06-23 14:15:34 -0500

    main file uploaded

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,26 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+class ErrorBoundary extends React.Component {
+  constructor(props) { super(props); this.state = { error: null } }
+  static getDerivedStateFromError(e) { return { error: e } }
+  render() {
+    if (this.state.error) {
+      return (
+        <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+          <h2 style={{ color: 'red' }}>Something went wrong</h2>
+          <pre style={{ fontSize: '12px', whiteSpace: 'pre-wrap', marginTop: '1rem' }}>
+            {this.state.error.toString()}
+          </pre>
+        </div>
+      )
+    }
+    return this.props.children
+  }
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <ErrorBoundary>
     <App />
-  </React.StrictMode>,
+  </ErrorBoundary>
 )
